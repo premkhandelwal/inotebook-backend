87582bce01a9094aefa624cb527f66cd433e56b5
Prem Khandelwal <prem.dkhandelwal@gmail.com>
2021-10-27 14:10:54 +0530

    Added custom messages for errors and created collections

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,19 +7,33 @@ router.post('/createUser', [
     body('name', 'Enter a valid name').isLength({ min: 3 }),
     body('email', 'Enter a valid email').isEmail(),
     body('password', 'Password must contain at least 5 characters').isLength({ min: 5 })
-], (req,res)=>{
+], async (req,res)=>{
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
+    
     }
-    let user =await User.create({
+
+    try {
+      let user = await  User.findOne({email:req.body.email});
+    if(user){
+        return res.status(400).json({error:"User already exists"})
+    }else{
+     user =await User.create({
         name: req.body.name,
-        email: req.body.password,
+        email: req.body.email,
         password: req.body.password,
-      })
+      })}
     //   .then(user => res.json(user)).catch(err=>{console.log(err);
     //   res.json({error: " Please enter a valid email" ,message: err.message})}
     //   );
+      res.json({"Nice": "nice"})
+    } catch (error) {
+      res.status(500).send("Some error occured")
+    }
+
+    
+
 });
 
 module.exports = router;
