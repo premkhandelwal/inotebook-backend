204938d9951ea282e537adbcbace6163a375d4c8
Prem Khandelwal <prem.dkhandelwal@gmail.com>
2021-10-29 12:19:56 +0530

    Completed CRUD operations on notes

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,9 +1,109 @@
-const express= require('express');
+const express = require('express');
 const router = express.Router();
+const fetchUser = require('../middleware/fetchUser');
+const Notes = require('../models/Notes');
+const { body, validationResult } = require('express-validator');
 
-router.get('/', (req,res)=>{
- 
-    res.json([]);
+
+
+router.get('/fetchAllNotes', fetchUser, async (req, res) => {
+    try {
+        const notes = await Notes.find({ user: req.user.id })
+        res.json(notes);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal server error");
+    }
+
+});
+
+router.post('/addNote', fetchUser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must contain at least 5 characters').isLength({ min: 5 })
+], async (req, res) => {
+    try {
+
+
+        const { title, description, tag } = req.body;
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+
+        }
+
+        const note = new Notes({
+            title, description, tag, user: req.user.id
+        });
+        const savedNote = await note.save();
+        res.json(savedNote);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal server error");
+    }
+});
+
+router.put('/updateNote/:id', fetchUser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must contain at least 5 characters').isLength({ min: 5 })
+], async (req, res) => {
+    try {
+
+
+        const { title, description, tag } = req.body;
+        const newNote = {}
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+
+        }
+        if (title) {
+            newNote.title = title;
+        }
+        if (description) {
+            newNote.description = description;
+        }
+        if (tag) {
+            newNote.tag = tag;
+        }
+
+        let note = await Notes.findById(req.params.id);
+        if (!note) {
+            return res.status(404).json({ errors: "Not Found" });
+
+        }
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not allowed");
+        }
+
+        note = await Notes.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+        res.json(note);
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal server error");
+    }
+});
+
+router.delete('/deleteNote/:id', fetchUser, [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Description must contain at least 5 characters').isLength({ min: 5 })
+], async (req, res) => {
+    try {
+        let note = await Notes.findById(req.params.id);
+        if (!note) {
+            return res.status(404).json({ errors: "Not Found" });
+
+        }
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("Not allowed");
+        }
+
+        note = await Notes.findByIdAndDelete(req.params.id);
+        res.json({ "Success": "Note has been deleted", note: note });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send("Internal server error");
+    }
 });
 
 module.exports = router;
